030bd7f6f1b8bb4581d304d817e363fbb8a7bb3d
Muhammad Usman Zia <usman.zia@dtechsystems.co>
2018-11-27 15:53:56 +0500

    init commit create project

---
+++ GITHubSearch/GITHubSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GITHubSearch
+//
+//  Created by Muhammad Usman Zia  on 11/27/18.
+//  Copyright © 2018 Muhammad Usman Zia . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
